4fc6447df4b6d1452089293ce655ff61dd58b875
TonyBNK <borisenk-anton@yandex.ru>
2021-09-29 14:41:05 +0300

    2. Step two is done.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,23 @@
 import React from "react";
-import c from "./Footer.module.css";
+import c from "./Footer.module.scss";
 import styles from '../../common/styles/Container.module.css'
+import {Title} from "../../common/components/title/Title";
+import telegram from '../../assets/icons/telegram.svg';
+import whatsApp from '../../assets/icons/whatsapp.svg';
+import instagram from '../../assets/icons/instagram.svg';
+import gmail from '../../assets/icons/gmail.svg';
 
 export const Footer = () => {
     return (
         <div className={c.footerBlock}>
             <div className={styles.container}>
-                <div className={c.header}>
-                    <h2>Tony</h2>
-                </div>
+                <Title text={'Tony'}/>
                 <div className={c.body}>
-                    <div className={c.form}>
-                        <img src="" alt=""/>
-                        <img src="" alt=""/>
-                        <img src="" alt=""/>
-                        <img src="" alt=""/>
+                    <div className={c.logos}>
+                        <img src={telegram} alt=""/>
+                        <img src={whatsApp} alt=""/>
+                        <img src={instagram} alt=""/>
+                        <img src={gmail} alt=""/>
                     </div>
                     <h4>©All rights reserved</h4>
                 </div>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,18 +3,26 @@ import React from "react";
 import {Project} from "./Project/Project";
 import styles from '../../common/styles/Container.module.css'
 import {Title} from "../../common/components/title/Title";
-import todolist from '../../assets/images/todolist.png';
+import todolist from '../../assets/images/todolist.jpg';
+import socialNetwork from '../../assets/images/socialNetwork.jpg';
+import counter from '../../assets/images/counter.jpg';
 
 export const Projects = () => {
     const todo = {
         backgroundImage: `url(${todolist})`
     }
+    const sn = {
+        backgroundImage: `url(${socialNetwork})`
+    }
+    const count = {
+        backgroundImage: `url(${counter})`
+    }
 
     const projects = [
         {id: 1, style: todo, title: 'Todolist', description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, praesentium!`},
-        {id: 2, style: null, title: 'Social Network JS', description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, praesentium!`},
-        {id: 3, style: null, title: 'Social Network TS', description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, praesentium!`},
-        {id: 4, style: null, title: 'Counter', description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, praesentium!`},
+        {id: 2, style: sn, title: 'Social Network JS', description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, praesentium!`},
+        {id: 3, style: sn, title: 'Social Network TS', description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, praesentium!`},
+        {id: 4, style: count, title: 'Counter', description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, praesentium!`},
     ];
 
     const projectsForBlock = projects.map(project =>
