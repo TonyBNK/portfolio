7c5f24fac9f52d8efe9407df88f05b08215c7c3d
TonyBNK <borisenk-anton@yandex.ru>
2021-10-23 17:11:47 +0300

    3. Added About page.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,47 +6,75 @@ export const About = () => {
     return (
         <div className={c.about}>
             <div className={c.aboutArea}>
-                <div className={c.aboutWrapper}>
-                    <div className={c.container}>
-                        <div className={c.row}>
-                            <div className={c.col}>
-                                <div className={c.inner}>
-                                    <div className={c.sectionTitle}>
-                                        <h2 className={c.title}>About Me</h2>
-                                        <div>
-                                            <div className={c.infoRow}>
-                                                <div className={c.infoCol1}>
-                                                    <div className={c.thumbnail}>
-                                                        <img src="" alt="About"/>
-                                                    </div>
-                                                </div>
-                                                <div className={c.infoCol2}>
-                                                    <p className={c.description}>
-                                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, unde!
-                                                    </p>
-                                                </div>
+                <div className={c.container}>
+                    <div className={c.row}>
+                        <div className={c.col}>
+                            <div className={c.sectionTitle}>
+                                <h2>About Me</h2>
+                                <div>
+                                    <div className={c.infoRow}>
+                                        <div className={c.infoCol1}>
+                                            <div className={c.thumbnail}>
+                                                <img src="" alt="About"/>
                                             </div>
-                                            <div className={c.experienceEducationRow}>
-                                                <div>
-                                                    <div className={c.tabsArea}>
-                                                        <div className={c.tabsContainer}>
-                                                            <div className={c.tabsRow}>
-                                                                <div className={c.tabsCol}>
-                                                                    <div className={c.reactTabs}>
-                                                                        <ul className={c.tabStyle}>
-                                                                            <li className={c.experience}>
-                                                                                Experience
-                                                                            </li>
-                                                                            <li className={c.education}>
-                                                                                Education
-                                                                            </li>
-                                                                        </ul>
-                                                                        <div></div>
-                                                                        <div></div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
+                                        </div>
+                                        <div className={c.infoCol2}>
+                                            <p>
+                                                Lorem ipsum dolor sit amet,
+                                                consectetur adipisicing elit.
+                                                Beatae, unde!
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className={c.experienceEducationRow}>
+                                        <div className={c.tabsContainer}>
+                                            <div className={c.tabsRow}>
+                                                <div className={c.tabsCol}>
+                                                    <div>
+                                                        <ul>
+                                                            <li className={`${c.active}`}>
+                                                                Experience
+                                                            </li>
+                                                            <li className={``}>
+                                                                Education
+                                                            </li>
+                                                        </ul>
+                                                        <div
+                                                            className={c.singleTabContent}>
+                                                            <ul>
+                                                                <li>
+                                                                    Lorem ipsum
+                                                                    dolor sit
+                                                                    amet,
+                                                                    consectetur
+                                                                    adipisicing
+                                                                    elit.
+                                                                    Dolorum,
+                                                                    ullam.
+                                                                </li>
+                                                                <li>
+                                                                    Lorem ipsum
+                                                                    dolor sit
+                                                                    amet,
+                                                                    consectetur
+                                                                    adipisicing
+                                                                    elit.
+                                                                    Dolorum,
+                                                                    ullam.
+                                                                </li>
+                                                                <li>
+                                                                    Lorem ipsum
+                                                                    dolor sit
+                                                                    amet,
+                                                                    consectetur
+                                                                    adipisicing
+                                                                    elit.
+                                                                    Dolorum,
+                                                                    ullam.
+                                                                </li>
+                                                            </ul>
                                                         </div>
+                                                        <div></div>
                                                     </div>
                                                 </div>
                                             </div>
